2e4e77add19668a3d4fe566255d844a518f4b745
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-11-25 09:11:51 -0300

    Update Aluno.java

--- ExceptionExer1/Entidades/Aluno.java
+++ ExceptionExer1/Entidades/Aluno.java
@@ -4,18 +4,16 @@ package Entidades;
  * declaramos atributos e metodos, que representam, respectivamente as 
  * caracteristicas e comportamentos desse objeto */
 public class Aluno {
-// Atributos nome(Conjunto de caracteres,  String) e idade(inteiro)
 	private String nome;
 	private int idade;
 	
-/* Construtor, que torna obrigatorio as informacoes para referenciar esse objeto */
+/* Construtores */
 	public Aluno(String nome, int idade) {
 		this.nome = nome;
 		this.idade = idade;
 	}
 	
 //Metodos
-	// metodo para exibir informacoes do aluno para o usuario
 	public String toString() {
 		System.out.println("Nome: "+nome);
 		System.out.println("Idade: "+idade);
